0268a53e1468f4507bd93048737ea8899a69ee9b
nastya24704 <nastazeravina@gmail.com>
2025-09-16 03:47:43 +0700

    Update task_6.py

--- task_6.py
+++ task_6.py
@@ -1,1 +1,9 @@
+def opposite_definition(x):
+    word = x.split(' ')
+    sentence = word[::-1]
+    opposite_sentence = ' '.join(sentence)
+    return opposite_sentence
 
+
+text = input()
+print(opposite_definition(text))
