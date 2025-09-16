55142f4d9705a603bb9cf82091cfbc728df5824b
nastya24704 <nastazeravina@gmail.com>
2025-09-16 11:14:29 +0700

    Update task_16.py

--- task_16.py
+++ task_16.py
@@ -1,15 +1,15 @@
 text = input()
-balance = 0
+a = 0
 
 for ch in text:
     if ch == '(':
-        balance += 1
+        a += 1
     elif ch == ')':
-        balance -= 1
-        if balance < 0:
+        a -= 1
+        if a < 0:
             break
 
-if balance == 0:
+if a == 0:
     print("Скобки расставлены правильно")
 else:
     print("Скобки расставлены неправильно")
