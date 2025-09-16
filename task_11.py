6bdbb54d0e913ca710239f4d74a9d62ff61847c7
nastya24704 <nastazeravina@gmail.com>
2025-09-16 10:35:27 +0700

    Create task_11.py

---
+++ task_11.py
@@ -0,0 +1,34 @@
+def winner(city):
+    cities = city.split()
+
+    for i in range(1, len(cities)):
+        previous_city = cities[i - 1].lower()
+        current_city = cities[i].lower()
+
+        last_char = last_letter(previous_city)
+
+        if current_city[0] != last_char:
+            if i % 2 == 1:
+                return "Петя"
+            else:
+                return "Вася"
+
+    if len(cities) % 2 == 1:
+        return "Петя"
+    else:
+        return "Вася"
+
+
+def last_letter(city):
+    excluded_chars = {'ь', 'ъ', 'ы'}
+
+    for i in range(len(city) - 1, -1, -1):
+        if city[i] not in excluded_chars:
+            return city[i]
+
+    return city[-1]
+
+
+cities_input = input().strip()
+winner = winner(cities_input)
+print(f"Победитель: {winner}")
