bae42d079ac21c359d3876a3e74bb4791fa04ebc
nastya24704 <nastazeravina@gmail.com>
2025-09-16 07:13:29 +0700

    Create task_15.py

---
+++ task_15.py
@@ -0,0 +1,40 @@
+def secret_number():
+    while True:
+        num = input("Ведущий, введите четырехзначное число с неповторяющимися цифрами: ")
+        if len(num) == 4 and len(set(num)) == 4:
+            return num
+        print("Ошибка! Число должно быть четырехзначным с неповторяющимися цифрами.")
+
+
+def all_bulls_cows(secret, guess):
+    bulls = cows = 0
+    for i in range(4):
+        if guess[i] == secret[i]:
+            bulls += 1
+        elif guess[i] in secret:
+            cows += 1
+    return bulls, cows
+
+
+def play_game():
+    secret = secret_number()
+    print("\n" * 25)
+    attempts = 10
+    print("Игра началась! Отгадайте число за 10 попыток.")
+    for attempt in range(1, attempts + 1):
+        while True:
+            guess = input(f"Попытка {attempt}. Введите ваш вариант: ")
+            if len(guess) == 4 and len(set(guess)) == 4:
+                break
+            print("Введите четырехзначное число с неповторяющимися цифрами.")
+
+        if guess == secret:
+            print("Победа!")
+            return
+        bulls, cows = all_bulls_cows(secret, guess)
+        print(f"Быков: {bulls} Коров: {cows}")
+
+    print(f"Проигрыш! Загаданное число было: {secret}")
+
+
+play_game()
